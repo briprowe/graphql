f81202efd2337b0ce43db806376a9a15002c4966
Brian Rowe <brianrowe@farmlogs.com>
2016-01-17 12:38:06 -0500

    No need for cantata yet.

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC5"]
-                 [cantata "0.1.17"]
                  [com.graphql-java/graphql-java "1.3"]])
